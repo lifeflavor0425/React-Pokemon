e5d941664ab327f8a3aa8ba49526b793a8d82b5b
lifeflavor0425 <rjsdnakfmzh61@gmail.com>
2024-01-24 15:50:51 +0900

    login page develop

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDRR0jhm3M_C7PEisXwaib-eo8yXqPaHFk",
+  authDomain: "react-poke-app-a9622.firebaseapp.com",
+  projectId: "react-poke-app-a9622",
+  storageBucket: "react-poke-app-a9622.appspot.com",
+  messagingSenderId: "965146173724",
+  appId: "1:965146173724:web:fceb0f674712a1b4cf5ad9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
